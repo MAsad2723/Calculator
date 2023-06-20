79c5a5eeb050e56a7ea9c27125e97efc0453fdcc
Asad <86630702+MAsad2723@users.noreply.github.com>
2023-06-20 21:36:34 +0500

    Added Complete Files

---
+++ script.js
@@ -0,0 +1,23 @@
+let string = ""
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == "=") {
+            string = eval(string);
+        }
+        else if (e.target.innerHTML == "AC") {
+            string = "";
+        }
+        else if (e.target.innerHTML == "+/-") {
+            string *= -1;
+        }
+        else if (e.target.innerHTML == "%") {
+            string /= 100;
+        }
+        else {
+            console.log(e.target);
+            string += e.target.innerHTML;
+        }
+        document.querySelector('.input').value = string;
+    })
+})
